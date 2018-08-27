59ca59e80982895fc1bde8fce99bec1171d504d5
Leon Lu <yanglu@microsoft.com>
2018-08-27 10:51:59 -0700

    demo: Branching and Looping

--- main.go
+++ main.go
@@ -3,17 +3,37 @@ package main
 import "fmt"
 
 func main(){
-	var plantCapacities []float64
+	plantCapacities := []float64{30,30,30,60,60,100}
 
-	plantCapacities = []float64{30,30,30,60,60,100}
+	activePlants := []int{0, 1}
 
-	var capacity float64 = plantCapacities[0] + plantCapacities[1] + plantCapacities[2] + plantCapacities[4] + plantCapacities[5]
+	gridLoad := 75.
 
-	var gridLoad = 75.
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Println("Please choose an option")
 
-	utilization := gridLoad / capacity
+	var option string
 
-	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
-	fmt.Printf("%-20s%.0f\n", "Load:", gridLoad)
-	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", utilization*100)
+	fmt.Scanln(&option)
+
+	switch option{
+	case "1":
+		for index, capacity := range plantCapacities{
+			fmt.Printf("Plant %d capacity: %.0f\n", index, capacity)
+		}
+	case "2":
+		capacity := 0.
+		for _, plantId := range activePlants{
+			capacity += plantCapacities[plantId]
+		}
+
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+
+	default:{
+		fmt.Printf("Unkown option, no action taken")
+	}
+	}
 }
